8f8406d01e4c8e1167d7e2b44194c9ebdc3173b7
StellaBellaO <101139955+StellaBellaO@users.noreply.github.com>
2022-07-27 22:22:00 +0100

    re-created site and design

--- script.js
+++ script.js
@@ -1,43 +1,14 @@
-let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
+function loader(){
+      document.querySelector('.loader-container').classList.add('fade-out');
+  }
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+  function fadeOut(){
+      setInterval(loader, 3000);
+  }
 
-let section = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header .navbar a')
+  window.onload = fadeOut();
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-
-    section.forEach(sec =>{
-
-        let top = window.scrollY;
-        let height = sec.offsetHeight;
-        let offset = sec.offsetTop - 150;
-        let id = sec.getAttribute('id');
-
-        if (top => offset && top < offset + height) {
-            navLinks.forEach(links =>{
-links.classList.remove('active');
-document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
-            });
-        };
-    });
-}
-
-document.querySelector('#search-icon'). onclick = () => {
-    document.querySelector('#search-form').classList.toggle('active');
-}
-
-document.querySelector('#close'). onclick = () => {
-    document.querySelector('#search-form').classList.remove('active');
-}
-
-var swiper = new Swiper(".home-slider", {
+  var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
@@ -50,15 +21,3 @@ var swiper = new Swiper(".home-slider", {
     },
     loop:true,
   });
-
-  function loader(){
-      document.querySelector('.loader-container').classList.add('fade-out');
-  }
-
-  function fadeOut(){
-      setInterval(loader, 3000);
-  }
-
-  window.onload = fadeOut();
-
- 
