c85cdd9d4ef5193b13b945b3bbd0380c4232983c
StellaBellaO <101139955+StellaBellaO@users.noreply.github.com>
2024-03-25 19:57:01 +0000

    last changes

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function loader(){
   document.querySelector("form.form-alert").addEventListener("submit", function() {
     alert("Your order has been placed.");
   }, false);
+  
+ // cart
+ let cartIcon = document.querySelector('#cart-icon');
+ let cart = document.querySelector('.cart');
+ let closeCart = document.querySelector('#close-cart');
+// Open Cart
+ cartIcon.onclick = () => {
+   cart.classList.add("active");
+ };
+// Close Cart
+ closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
